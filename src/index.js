f48330cf9fc33ebc29ba6df4521205adf698a07d
Oleg Nykerui <nykeruyoleg96@gmail.com>
2022-10-27 16:41:04 +0300

    fix index js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import { ToastContainer } from 'react-toastify';
+ß;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <ToastContainer />
   </React.StrictMode>
 );
