aa7188db25176cee30352a2b0ec1843381795518
Oleg Nykerui <nykeruyoleg96@gmail.com>
2022-10-27 23:30:50 +0300

    update App.jsx (hooks)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,96 +1,79 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import nextId from 'react-id-generator';
 
 import ContactsList from './Contacts/ContactsList';
 import ContactForm from './Form/ContactForm';
 import Filter from './Filter/Filter';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+const abonents = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
-  componentDidMount() {
-    const getName = localStorage.getItem('name');
-    const parcedName = JSON.parse(getName);
-    if (parcedName) {
-      this.setState({
-        contacts: parcedName,
-      });
-    }
-  }
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) || abonents
+  );
 
-  componentDidUpdate(PrevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('name', JSON.stringify(this.state.contacts));
-    }
-  }
+  const [filter, setFilter] = useState('');
 
-  addContact = ({ number, name }) => {
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = ({ number, name }) => {
     if (
-      this.state.contacts.find(
+      contacts.find(
         contact => contact.name.toLowerCase() === name.toLowerCase()
       )
     ) {
       alert(`${name} is already in contacts`);
     } else {
-      this.setState(prevState => {
-        return {
-          contacts: [
-            ...prevState.contacts,
-            { id: nextId(), name: name, number: number },
-          ],
-        };
-      });
+      setContacts(prevState => [
+        ...prevState,
+        { id: nextId(), name: name, number: number },
+      ]);
     }
   };
 
-  filterChange = event => {
-    this.setState({ filter: event.target.value });
+  const filterChange = event => {
+    setFilter(event.target.value);
   };
 
-  deleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
+  const deleteContact = id => {
+    setContacts(prevState => prevState.filter(contact => contact.id !== id));
   };
 
-  getFilteredContacts = () => {
-    const filterName = this.state.filter.toLowerCase();
-    return this.state.contacts.filter(contact =>
+  const getFilteredContacts = () => {
+    const filterName = filter.toLowerCase();
+    return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filterName)
     );
   };
 
-  render() {
-    const { filter } = this.state;
-    const fltered = this.getFilteredContacts();
+  const fltered = getFilteredContacts();
 
-    return (
-      <div
-        style={{
-          width: '500px',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-          border: '1px dashed orange',
-          backgroundColor: '#fffcf9',
-        }}
-      >
-        <h1 style={{ textAlign: 'center' }}>PhoneBook</h1>
+  return (
+    <div
+      style={{
+        width: '500px',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        border: '1px dashed orange',
+        backgroundColor: '#fffcf9',
+      }}
+    >
+      <h1 style={{ textAlign: 'center' }}>PhoneBook</h1>
 
-        <ContactForm onSubmit={this.addContact} />
+      <ContactForm onSubmit={addContact} />
 
-        <h2 style={{ textAlign: 'center' }}>Contacts</h2>
+      <h2 style={{ textAlign: 'center' }}>Contacts</h2>
 
-        <Filter value={filter} onChange={this.filterChange} />
-        <ContactsList deleteButton={this.deleteContact} contacts={fltered} />
-      </div>
-    );
-  }
-}
+      <Filter value={filter} onChange={filterChange} />
+      <ContactsList deleteButton={deleteContact} contacts={fltered} />
+    </div>
+  );
+};
+export default App;
